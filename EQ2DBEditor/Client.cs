72c07fd4f4d20b169deec359fd976644861cd8a9
Cynnar <jacob0305@gmail.com>
2022-11-05 16:47:19 -0500

    Initial Commit Nov 2022

---
+++ EQ2DBEditor/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace EQ2DBEditor
+{
+    public enum httpVerb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    class Client
+    {
+        public string endPoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+
+        public Client()
+        {
+            endPoint = string.Empty;
+            httpMethod = httpVerb.GET;
+        }
+
+        public string makeRequest()
+        {
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = httpMethod.ToString();
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("Error Code: " + response.StatusCode.ToString());
+                }
+                // Process the response stream...
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return strResponseValue;
+        }
+    }
+}
